e0d556648d52247dd71d32f34ad21d4136fc7632
unknown <meowskyj@yandex.ru>
2023-10-16 18:15:46 +0300

    feat: add

--- src/components/Projects/ProjectPage/ProjectPage.js
+++ src/components/Projects/ProjectPage/ProjectPage.js
@@ -6,18 +6,26 @@ import './ProjectPage.css';
 const ProjectPage = () => {
     const params = useParams();
     const id = params.projectId;
+    const project = projectData[id];
     return (<div className="container">
         {console.log(projectData)}
         <div className="project-page">
-            <div>
-                <h3 className="project-page__name">
-                    {projectData[id].name}
-                </h3>
-                <p>{projectData[id].title}</p>
+            <div className="project-page__header">
+                <div className="project-page__info">
+                    <h3 className="project-page__name">{project.name}</h3>
+                    <p>{project.title}</p>
+                </div>
+                <img className="project-page__img" src={require(`../../../data/${project.id}/prew.jpg`)} alt="" />
+            </div>
+            <hr />
+            <div className='project-page__grid'>
+
+                {project.photos.map((photo, index) =>
+                    <img className="project-page__photo" src={require(`../../../data/${project.id}/${photo}.png`)} alt="" />
+
+                )}
             </div>
 
-            <img className="project-page__img" src={require(`../../../data/${projectData[id].id}/prew.jpg`)} alt="" />
-            <hr />
         </div>
 
     </div>);
