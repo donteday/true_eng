78a8ba9d4d0ce6966ed3472ac97a9ab01d13cc5d
unknown <meowskyj@yandex.ru>
2023-10-19 17:00:26 +0300

    feat: add price

--- src/components/Projects/ProjectsSlider/ProjectsSlider.js
+++ src/components/Projects/ProjectsSlider/ProjectsSlider.js
@@ -12,22 +12,17 @@ const ProjectsSlider = () => {
         e.target.className.includes('left') ?
         sliderRef.current.scrollLeft-=475
         : sliderRef.current.scrollLeft+=475;
-        // sliderRef.offsetLeft = 200;
     }
 
     return (
         <div className='container projects'>
-
             <span className='projects__header'>Проекты<hr /></span>
             <div className='projects__slider'>
                 <div className='projects__slider-arrow left' onClick={arrowHandeler}>&#10094;</div>
-
                 <div className='projects__box' ref={sliderRef}>
                     {projectData.map((e, index) =>
                         <Link to={`/projects/${index}`} relative="path" className='projects__item'>
-
                             <img className='projects__item-img' src={require(`../../../data/${e.id}/prew.jpg`)} alt="" />
-
                             <h3 to={`projects/${index}`} className='projects__item-name'>{e.name}</h3>
                             <p className='projects__item-title'>{e.title}</p>
                         </Link>
--- src/index.js
+++ src/index.js
@@ -37,7 +37,7 @@ const router = createBrowserRouter([
         element: <Contacts/>,
       },
       {
-        path: "consult",
+        path: "services/:serviceId",
         element: <ServicesItem />,
       },
       {
